7a2a23131f0b48901bbcfc4bbc25ae2f5927b18c
Frank Kyei Baffour <frankkyeibaffour@gmail.com>
2023-09-29 20:02:11 +0000

    Done with Task 8

--- app/manual_rating_and_performace_evaluation.py
+++ app/manual_rating_and_performace_evaluation.py
@@ -48,3 +48,43 @@ with open("evaluation.csv", "r", newline="", encoding="utf-8") as file:
             print("\n" + "=" * 40 + "\n")
 
 print("Ratings have been saved to evaluation_rated.csv")
+
+# Calculating Accuracy for Top-1 and Top-3
+# Load the rated CSV file (evaluation_rated.csv)
+with open("evaluation_rated.csv", "r", newline="", encoding="utf-8") as rated_file:
+    reader = csv.DictReader(rated_file)
+    
+    # Initialize variables to count top-1 and top-3 correct answers
+    top_1_correct = 0
+    top_3_correct = 0
+
+    for row in reader:
+        question = row["Question"]
+        is_relevant_1 = row["Is Passage 1 Relevant?"].strip().lower()  # Convert to lowercase for comparison
+        is_relevant_2 = row["Is Passage 2 Relevant?"].strip().lower()
+        is_relevant_3 = row["Is Passage 3 Relevant?"].strip().lower()
+        
+        # Check if the first passage is relevant (top-1)
+        if is_relevant_1 == "yes":
+            top_1_correct += 1
+        
+        # Check if at least one of the top-3 passages is relevant (top-3)
+        if is_relevant_1 == "yes" or is_relevant_2 == "yes" or is_relevant_3 == "yes":
+            top_3_correct += 1
+
+# Calculate the top-1 and top-3 accuracy percentages
+total_queries = 10
+top_1_accuracy = (top_1_correct / total_queries) * 100
+top_3_accuracy = (top_3_correct / total_queries) * 100
+
+# Save the accuracy results to a new CSV file (performance.csv)
+with open("performance.csv", "w", newline="", encoding="utf-8") as performance_file:
+    writer = csv.writer(performance_file)
+    
+    # Write the header row
+    writer.writerow(["Top 1 Accuracy", "Top 3 Accuracy"])
+    
+    # Write the accuracy values
+    writer.writerow([top_1_accuracy, top_3_accuracy])
+
+print("Performance metrics have been saved to performance.csv")
